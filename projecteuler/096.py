c8cf0db2313dca0b6cdd7c24aa65afa9ae346503
knotsofunny <knotsofunny@gmail.com>
2016-11-18 15:53:20 -0500

    Continued work on problem 96

--- projecteuler/096.py
+++ projecteuler/096.py
@@ -1,14 +1,16 @@
-test = [[0,0,3,0,2,0,6,0,0],
-        [9,0,0,3,0,5,0,0,1],
-        [0,0,1,8,0,6,4,0,0],
-        [0,0,8,1,0,2,9,0,0],
-        [7,0,0,0,0,0,0,0,8],
-        [0,0,6,7,0,8,2,0,0],
-        [0,0,2,6,0,9,5,0,0],
-        [8,0,0,2,0,3,0,0,9],
-        [0,0,5,0,1,0,3,0,0]]
+import math
+#        0 1 2 3 4 5 6 7 8
+test = [[0,0,3,0,2,0,6,0,0],#0
+        [9,0,0,3,0,5,0,0,1],#1
+        [0,0,1,8,0,6,4,0,0],#2
+        [0,0,8,1,0,2,9,0,0],#3
+        [7,0,0,0,0,0,0,0,8],#4
+        [0,0,6,7,0,8,2,0,0],#5
+        [0,0,2,6,0,9,5,0,0],#6
+        [8,0,0,2,0,3,0,0,9],#7
+        [0,0,5,0,1,0,3,0,0]]#8
     
-#Returns a list of coordinates of a given number in a given array
+#Returns a list of coordinates of all instances of a given number in a given array
 def findBlocked(array, number):
     blocked=[]
     for x in range(0,9):
@@ -17,12 +19,26 @@ def findBlocked(array, number):
                 blocked.append([x,y])
                 break
     return blocked
-   
-#
-def checkBox(array, number, coordinate):
-    box = []
 
-        
+#Returns the midpoint of box containing given coordinates
+def findBox(coordinate):
+    box = [0,0]
+    box[0] = math.ceil((coordinate[0] + 1) / 3) * 3 - 2
+    box[1] = math.ceil((coordinate[1] + 1) / 3) * 3 - 2
+    
+    return box    
+    
+#Returns false if box containing given coordinate contains given number
+def checkBox(array, number, coordinate):
+    box = findBox(coordinate)
+    for x in range(box[0] - 1, box[0] + 2):
+        for y in range(box[1] - 1, box[1] + 2):
+            if array[y][x] == number:
+                return False
+    return True
+
+
+       
 
 
 '''
